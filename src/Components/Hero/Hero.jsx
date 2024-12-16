81170d7536c37d73217bc7c01c3ff1d3ddf58994
suleman37 <sulemanshakeel37@gmail.com>
2024-12-16 13:18:44 +0500

    Update Hero UI

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,25 +3,29 @@ import Seperator from "../../images/separator.svg";
 import { motion } from "framer-motion";
 import "./Hero.css";
 
+const fontFamily = "'Roboto', sans-serif"; // Define the font family
+
 const Hero = () => {
   return (
      <>
-      <div className="container-fluid bg">
+      <div className="container-fluid bg" style={{ fontFamily: fontFamily }}>
         <div className="row">
           <div className="col">
             <motion.div animate={{ scale: 1 }} initial={{ scale: 0 }}>
-              <p id="main-head">
+              <p id="main-head" style={{ fontFamily: fontFamily }}>
                 <small>DELIGHTFUL EXPERIENCE</small>
               </p>
               <img src={Seperator} alt="" id="seperator" width="100px" />
-              <p id="main-2-head">
+              <p id="main-2-head" style={{ fontFamily: fontFamily }}>
+                <b>
                 Flavours Inspired by
                 <br />
                 the Seasons
+                </b>
               </p>
-              <p>Come with the Family & feel the joy of mouthwatering food</p>
+              <p style={{ fontFamily: fontFamily }}>Come with the Family & feel the joy of mouthwatering food</p>
               <a href="#menu">
-                <button type="button" id="btn2" className="">
+                <button type="button" id="btn2" className="" style={{ fontFamily: fontFamily }}>
                   <small>View our Menu</small>
                 </button>
               </a>
